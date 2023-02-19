a3475a43fa9f576670753623f480e762808284fa
Diganta Chaudhuri <digantachaudhuri03@gmail.com>
2023-02-19 17:19:33 +0530

    Env Added

--- database/db.go
+++ database/db.go
@@ -2,10 +2,9 @@ package database
 
 import (
 	"fmt"
+	"log"
+	"os"
 
-	"github.com/DigantaChauduri06/commons"
-	"github.com/DigantaChauduri06/models"
-	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -13,17 +12,19 @@ import (
 var DB *gorm.DB
 
 func SetUp() {
+	
+	
 	host := "localhost:5432"
 	port := "5432"
 	dbName := "book"
-	username := "postgres"
-	password := "Diganta@06"
+	username := os.Getenv("username")
+	password := os.Getenv("password")
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, username, password, dbName, port)
-
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	commons.CheckError(err)
-	db.AutoMigrate(models.Book{})
-	DB = db
+	log.Println(dsn)
+	// db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// commons.CheckError(err)
+	// db.AutoMigrate(models.Book{})
+	// DB = db
 }
 
 func GetDB() *gorm.DB {
--- main.go
+++ main.go
@@ -1,4 +1,13 @@
 package main
 
+import (
+	"github.com/DigantaChauduri06/commons"
+	"github.com/DigantaChauduri06/database"
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	err := godotenv.Load(".env")
+	commons.CheckError(err)
+	database.SetUp()
 }
